9df4f7a52fa580f5760a2867fa2f7f8728608a78
Efer482 <64799814+Efer482@users.noreply.github.com>
2022-12-10 20:52:05 -0500

    Prueba2022

--- public/components/src/app/components/form/form.component.ts
+++ public/components/src/app/components/form/form.component.ts
@@ -6,6 +6,7 @@ import { TableComponent } from '../table/table.component';
   selector: 'app-form',
   templateUrl: './form.component.html',
 })
+
 export class FormComponent {
   
   constructor(private caracteristicasService: CaracteristicasService, private viewContainer: ViewContainerRef) {
@@ -15,16 +16,16 @@ export class FormComponent {
   
   form = new FormGroup({
     // Create a FormControl instance for each form field.
-    gce_nombre_equipo: new FormControl('ds', [Validators.required]),
-    gce_board: new FormControl('sad', [Validators.required]),
-    gce_case: new FormControl('das', [Validators.required]),
-    gce_procesador: new FormControl('das', [Validators.required]),
-    gce_grafica: new FormControl('dad', [Validators.required]),
-    gce_ram: new FormControl('s', [Validators.required]),
-    gce_disco_duro: new FormControl('dsa', [Validators.required]),
-    gce_teclado: new FormControl('das', [Validators.required]),
-    gce_mouse: new FormControl('dsa', [Validators.required]),
-    gce_pantalla: new FormControl('dsa', [Validators.required]),
+    gce_nombre_equipo: new FormControl('', [Validators.required]),
+    gce_board: new FormControl('', [Validators.required]),
+    gce_case: new FormControl('', [Validators.required]),
+    gce_procesador: new FormControl('', [Validators.required]),
+    gce_grafica: new FormControl('', [Validators.required]),
+    gce_ram: new FormControl('', [Validators.required]),
+    gce_disco_duro: new FormControl('', [Validators.required]),
+    gce_teclado: new FormControl('', [Validators.required]),
+    gce_mouse: new FormControl('', [Validators.required]),
+    gce_pantalla: new FormControl('', [Validators.required]),
     gce_estado: new FormControl('1', [Validators.required]),
   });
 
@@ -32,6 +33,21 @@ onSubmitForm()  {
   this.caracteristicasService.add(this.form.value).subscribe((resp : any)=>{
     this.viewContainer.clear();
     this.viewContainer.createComponent(TableComponent);
+    this.form = new FormGroup({
+      // Create a FormControl instance for each form field.
+      gce_nombre_equipo: new FormControl('', [Validators.required]),
+      gce_board: new FormControl('', [Validators.required]),
+      gce_case: new FormControl('', [Validators.required]),
+      gce_procesador: new FormControl('', [Validators.required]),
+      gce_grafica: new FormControl('', [Validators.required]),
+      gce_ram: new FormControl('', [Validators.required]),
+      gce_disco_duro: new FormControl('', [Validators.required]),
+      gce_teclado: new FormControl('', [Validators.required]),
+      gce_mouse: new FormControl('', [Validators.required]),
+      gce_pantalla: new FormControl('', [Validators.required]),
+      gce_estado: new FormControl('1', [Validators.required]),
+    });
   })
   }
+  
 }
